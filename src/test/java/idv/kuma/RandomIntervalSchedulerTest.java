4a8c13a0f0307f8ae0f1a2755b052a71dd25e381
Yu-Song Syu <kuma@simbo.com.tw>
2020-04-01 13:13:12 +0800

    normal case test

--- src/test/java/idv/kuma/RandomIntervalSchedulerTest.java
+++ src/test/java/idv/kuma/RandomIntervalSchedulerTest.java
@@ -1,11 +1,48 @@
 package idv.kuma;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({RandomUtils.class})
 public class RandomIntervalSchedulerTest {
+    @Test
+    public void Schedule_With_Random_Number() {
+
+        ScheduledExecutorService mockedService = Mockito.mock(ScheduledExecutorService.class);
+
+        Runnable mockedRunnable = Mockito.mock(Runnable.class);
+
+        RandomIntervalScheduler.Range firstRange = new RandomIntervalScheduler.Range(1, 2);
+        RandomIntervalScheduler.Range secondRange = new RandomIntervalScheduler.Range(3, 4);
+
+        RandomIntervalScheduler randomIntervalScheduler = new RandomIntervalScheduler(
+                mockedService,
+                mockedRunnable,
+                firstRange,
+                secondRange
+        );
+
+        randomIntervalScheduler.trigger();
+
+        Mockito.verify(mockedService).schedule(any(Runnable.class), eq(1L), eq(TimeUnit.SECONDS));
+
+
+    }
+
+
     @Test
     public void name() throws InterruptedException {
 
